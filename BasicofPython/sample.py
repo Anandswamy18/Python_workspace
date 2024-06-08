9507f64ca8b99b9dbee4fd6f5c9c7ba37dbd0812
Anandswamy18 <anandsmathpati18@gmail.com>
2024-06-08 19:05:31 +0530

    [Anand] intial commi

---
+++ BasicofPython/sample.py
@@ -0,0 +1,44 @@
+message="hi hello welcome to python"
+print(message);
+
+print('------------------------------------------------')
+
+help_message = '''
+Usage: mysql command
+    -h hostname     
+    -d database name
+    -u username
+    -p password 
+'''
+print(help_message);
+
+
+
+print('------------------------------------------------')
+name='anand'
+fullname= f'{name} swamy'
+print(fullname)
+
+
+name='anand'
+lastname='swamy'
+fullname=name+lastname
+print(fullname)
+
+print('------------------------------------------------')
+
+string="anand swamy"
+print(string[9])
+print(string[-11])
+
+print('------------------------------------------------')
+
+string="anand swamy"
+print(len(string))
+
+
+
+print('------------------------------------------------')
+
+str="Bengaluru"
+print(str[-1:0])
