e590500fe2fa4cdfea1c62c5db5ace9feedfad0d
Anandswamy18 <anandsmathpati18@gmail.com>
2024-06-11 16:27:17 +0530

    [Anand] added Dictionary Problems

---
+++ ML_Problems/DictionaryProblems/AddKeyToExistingDict.py
@@ -0,0 +1,10 @@
+dict={"name":"anand","lastname":"swamy"}
+
+print("before adding new key",dict)
+
+
+dict["job"]="Frontend Dev"
+
+print("after adding new key",dict)
+
+
---
+++ ML_Problems/DictionaryProblems/concatingDict.py
@@ -0,0 +1,13 @@
+
+dic1 = {"Scholl": "GHS", "Address": "Dongargaon"}
+dic2 = {"StudentName": "Anand", "FatherName": "Sharanayya"}
+dic3 = {"Class": 10, "percentage": 80}
+
+
+result_dict = {}
+result_dict.update(dic1)
+result_dict.update(dic2)
+result_dict.update(dic3)
+
+
+print("Concatenated Dictionary:", result_dict)
