f9ff599d26dc833ef66424a5e6d90e271f89d73e
Anandswamy18 <anandsmathpati18@gmail.com>
2024-06-12 15:02:41 +0530

    [Anand] added set based problems

---
+++ ML_Problems/Sets_Problems/AddindNewElement.py
@@ -0,0 +1,14 @@
+my_set = {1, 2, 3}
+print("Set before adding a single element:", my_set)
+
+my_set.add(4)
+
+
+print("Set after adding a single element:", my_set)
+
+print("Set before adding a single element:", my_set)
+
+
+my_set.update([5, 6, 7])
+
+print("Set after adding multiple elements:", my_set)
