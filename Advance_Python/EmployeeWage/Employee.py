76755495eb1f01879ed2784173c8fce84cee4a92
Anandswamy18 <anandsmathpati18@gmail.com>
2024-06-14 13:59:41 +0530

    [Anand] completed UC5

--- Advance_Python/EmployeeWage/Employee.py
+++ Advance_Python/EmployeeWage/Employee.py
@@ -1,10 +1,9 @@
 import random
 
 def display_welcome_message():
-    print("Welcome to Employee Wage Computation Program")
+    print("Welcome to Employee Wage Computation Program ")
 
 def check_attendance():
-    
     return random.choice([0, 1, 2])
 
 def calculate_daily_wage(attendance):
@@ -12,16 +11,26 @@ def calculate_daily_wage(attendance):
     full_day_hours = 8
     part_time_hours = 4
 
-    switch = {
-        0: lambda: print("Employee is Absent. No Wage Earned."),
-        1: lambda: print(f"Employee is Full-time Present. Daily Wage: Rs {wage_per_hour * full_day_hours}"),
-        2: lambda: print(f"Employee is Part-time Present. Daily Wage: Rs {wage_per_hour * part_time_hours}")
-    }
+    if attendance == 0:
+        return 0  
+    elif attendance == 1:
+        return wage_per_hour * full_day_hours  
+    elif attendance == 2:
+        return wage_per_hour * part_time_hours  
 
-    
-    switch.get(attendance, lambda: None)()
+def calculate_monthly_wage():
+    total_working_days = 20
+    total_wage = 0
+
+    for day in range(1, total_working_days + 1):
+        attendance = check_attendance()
+        daily_wage = calculate_daily_wage(attendance)
+        total_wage += daily_wage
+        print(f"Day {day}: Daily Wage: Rs {daily_wage}")
+
+    print(f"\nTotal Monthly Wage: Rs{total_wage}")
 
 
 display_welcome_message()
-attendance = check_attendance()
-calculate_daily_wage(attendance)
+calculate_monthly_wage()
+
