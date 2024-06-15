12c41787fff27f26daffe0fbccdac43b8a857ffc
Anandswamy18 <anandsmathpati18@gmail.com>
2024-06-15 18:43:02 +0530

    [Anand] completed UC5

--- Advance_Python/SnakeAndLadder/Snake_Ladder.py
+++ Advance_Python/SnakeAndLadder/Snake_Ladder.py
@@ -1,5 +1,6 @@
 import random
 
+
 WINNING_POSITION = 100
 
 def roll_dice():
@@ -13,12 +14,15 @@ def game():
     while player_position < WINNING_POSITION:
         dice_value = roll_dice()
         options = check_options()
+        
         if options == 'No play':
             pass
         elif options == 'Ladder':
-            player_position += dice_value
+             if player_position + dice_value <= WINNING_POSITION:
+                player_position += dice_value
         else:
             player_position -= dice_value
+
         if player_position < 0:
             player_position = 0
     return player_position
