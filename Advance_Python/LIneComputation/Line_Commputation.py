ec1670dd9ef70b192c3a9fe9b03952f5d8b8d5ca
Anandswamy18 <anandsmathpati18@gmail.com>
2024-06-16 18:31:31 +0530

    [Anand] UC3 completed

--- Advance_Python/LIneComputation/Line_Commputation.py
+++ Advance_Python/LIneComputation/Line_Commputation.py
@@ -1,26 +1,26 @@
 import math
 
-print("welcome to line comparison computation")
-
 def calculate_length(x1, y1, x2, y2):
-    
+   
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-def are_lines_equal(x1, y1, x2, y2, x3, y3, x4, y4):
-  
+def compare_lines(x1, y1, x2, y2, x3, y3, x4, y4):
     
     length1 = calculate_length(x1, y1, x2, y2)
     length2 = calculate_length(x3, y3, x4, y4)
     
-  
-    return length1 == length2
+    if length1 < length2:
+        return "third line is shorter"
+    elif length1 > length2:
+        return "first line is longer"
+    else:
+        return "lines are equal"
 
-x1, y1, x2, y2 = 3, 4, 7, 1  
+x1, y1, x2, y2 = 3, 4, 7, 1 
 x3, y3, x4, y4 = 3, 4, 7, 1  
-x5, y5, x6, y6 = 7, 1, 3, 4  
-x7, y7, x8, y8 = 2, 3, 6, 5  
+x5, y5, x6, y6 = 2, 3, 6, 5 
 
-# Check equality
-print(are_lines_equal(x1, y1, x2, y2, x3, y3, x4, y4))  
-print(are_lines_equal(x1, y1, x2, y2, x5, y5, x6, y6))  
-print(are_lines_equal(x1, y1, x2, y2, x7, y7, x8, y8))  
+
+print(compare_lines(x1, y1, x2, y2, x3, y3, x4, y4))  
+print(compare_lines(x1, y1, x2, y2, x5, y5, x6, y6))  
+print(compare_lines(x5, y5, x6, y6, x1, y1, x2, y2))  
